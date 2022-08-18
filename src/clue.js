33608a411a076f5859d57f5b790c8ac3de9b95a6
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2022-08-18 17:38:02 -0400

    Solved lab

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,203 @@
 
 // Suspects Array
 
-const suspectsArray = [];
-
+const suspectsArray = [
+    {
+        firstName: 'Jacob',
+        lastName: 'Green',
+        occupation: 'Entrepreneur',
+        age: 45,
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green'
+    },
+    {
+        firstName: 'Doctor',
+        lastName: 'Orchid',
+        occupation: 'Scientist',
+        age: 26,
+        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'white'
+    },
+    {
+        firstName: 'Victor',
+        lastName: 'Plum',
+        occupation: 'Designer',
+        age: 22,
+        description: 'Billionaire video game designer',
+        image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+        color: 'purple'
+    },
+    {
+        firstName: 'Kasandra',
+        lastName: 'Scarlet',
+        occupation: 'Actor',
+        age: 31,
+        description: 'She is an A-list movie star with a dark past',
+        image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'red'
+    },
+    {
+        firstName: 'Eleanor',
+        lastName: 'Peacock',
+        occupation: 'Socialite',
+        age: 36,
+        description: 'She is from a wealthy family and uses her status and money to earn popularity',
+        image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+        color: 'blue'
+    },
+    {
+        firstName: 'Jack',
+        lastName: 'Mustard',
+        occupation: 'Retired Football player',
+        age: 62,
+        description: 'he is a former football player who tries to get by on his former glory',
+        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+        color: 'yellow'
+    }
+];
+let mrGreen = suspectsArray[0];
+let drOrchid = suspectsArray[1];
+let profPlum = suspectsArray[2];
+let missScarlet = suspectsArray[3];
+let missPeacock = suspectsArray[4];
+let mrMustard = suspectsArray[5];
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {
+        name: 'Dining Room'
+    },
+    {
+        name: 'Conservatory'
+    },
+    {
+        name: 'Kitchen'
+    },
+    {
+        name: 'Study'
+    },
+    {
+        name: 'Library'
+    },
+    {
+        name: 'Billiard Room'
+    },
+    {
+        name: 'Lounge'
+    },
+    {
+        name: 'Ballroom'
+    },
+    {
+        name: 'Hall'
+    },
+    {
+        name: 'Spa'
+    },
+    {
+        name: 'Living Room'
+    },
+    {
+        name: 'Observatory'
+    },
+    {
+        name: 'Theater'
+    },
+    {
+        name: 'Guest House'
+    },
+    {
+        name: 'Patio'
+    }
+];
+let diningRoom = roomsArray[0];
+let conservatory = roomsArray[1];
+let kitchen = roomsArray[2];
+let study = roomsArray[3];
+let library = roomsArray[4];
+let billiardRoom = roomsArray[5];
+let lounge = roomsArray[6];
+let ballroom = roomsArray[7];
+let hall = roomsArray[8];
+let spa = roomsArray[9];
+let livingRoom = roomsArray[10];
+let observatory = roomsArray[11];
+let theater = roomsArray[12];
+let guestHouse = roomsArray[13];
+let patio = roomsArray[14];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {
+        name: 'rope',
+        weight: 10
+    },
+    {
+        name: 'knife',
+        weight: 8
+    },
+    {
+        name: 'candlestick',
+        weight: 8
+    },
+    {
+        name: 'dumbbell',
+        weight: 30
+    },
+    {
+        name: 'poison',
+        weight: 2
+    },
+    {
+        name: 'axe',
+        weight: 15
+    },
+    {
+        name: 'bat',
+        weight: 13
+    },
+    {
+        name: 'trophy',
+        weight: 24
+    },
+    {
+        name: 'pistol',
+        weight: 20
+    }
+];
+let rope = weaponsArray[0];
+let knife = weaponsArray[1];
+let candlestick = weaponsArray[2];
+let dumbbell = weaponsArray[3];
+let poison = weaponsArray[4];
+let axe = weaponsArray[5];
+let bat = weaponsArray[6];
+let trophy = weaponsArray[7];
+let pistol = weaponsArray[8]
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(anArray) {
+    let random = Math.floor(Math.random() * anArray.length)
+    return anArray[random];
+}
 
-function pickMystery() {}
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
 
